f87c7e7c7006239b0b9e8bcbc6f4246aea684e96
Hirohito <211102@st.yoshida-g.ac.jp>
2022-12-26 11:46:39 +0900

    ゲーム画面の変更と手のテクスチャ

--- HGS/game.cpp
+++ HGS/game.cpp
@@ -34,7 +34,41 @@ CGame::~CGame()
 HRESULT CGame::Init()
 {
 
+	//GetUiData
+	m_pBg = new CUI;
+
+	//UiInit
+	if (FAILED(m_pBg->Init()))
+	{
+		return -1;
+	}
+
+	//GetUiPointer
+	C2DPolygon *pPolygon = m_pBg->CreateUi(3);
+
+	//SetUiData
+	int nIndex = CTexture::LoadTexture("data/TEXTURE/sky.png");
+	pPolygon[0].SetTextIndex(nIndex);
+	pPolygon[0].SetPos(D3DXVECTOR3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0.0f));
+	pPolygon[0].SetDiagonalLine(SCREEN_WIDTH, SCREEN_HEIGHT);
+	pPolygon[0].SetPolygon();
+
+	nIndex = 0;
+	pPolygon[1].SetTextIndex(nIndex);
+	pPolygon[1].SetPos(D3DXVECTOR3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f + 30.0f, 0.0f));
+	pPolygon[1].SetDiagonalLine(SCREEN_WIDTH, 20.0f);
+	pPolygon[1].SetColor(D3DXCOLOR(0.4f,1.0f,1.0f,1.0f));
+	pPolygon[1].SetPolygon();
+
+	nIndex = CTexture::LoadTexture("data/TEXTURE/sea.png");
+	pPolygon[2].SetTextIndex(nIndex);
+	pPolygon[2].SetPos(D3DXVECTOR3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0.0f));
+	pPolygon[2].SetDiagonalLine(SCREEN_WIDTH, SCREEN_HEIGHT);
+	pPolygon[2].SetUVMove(D3DXVECTOR2(0.0005f, 0.0f));
+	pPolygon[2].SetPolygon();
+
 	return S_OK;
+
 }
 
 //*****************************************************************************
@@ -42,6 +76,12 @@ HRESULT CGame::Init()
 //*****************************************************************************
 void CGame::Uninit()
 {
+	if (m_pBg != nullptr)
+	{
+		m_pBg->Uninit();
+		delete m_pBg;
+		m_pBg = nullptr;
+	}
 }
 
 //*****************************************************************************
@@ -49,6 +89,7 @@ void CGame::Uninit()
 //*****************************************************************************
 void CGame::Update()
 {
+	m_pBg->Update();
 
 	//GetInput
 	CInput *pInput = CInput::GetKey();
@@ -66,4 +107,5 @@ void CGame::Update()
 //*****************************************************************************
 void CGame::Draw()
 {
+	m_pBg->Draw();
 }
